ca6f48f6a3761cf11b377b2774c0b48c8e3514ed
shuisheng <2274751790@qq.com>
2022-09-28 03:04:08 +0800

    feat: 添加默认导出

--- packages/components/index.ts
+++ packages/components/index.ts
@@ -1,5 +1,6 @@
+import * as components from './components';
 import { version as __VERSION__ } from './package.json';
 
 export const version = __VERSION__;
 
-export * from './src/ScratchCanvas';
+export default components;
--- packages/utils/index.ts
+++ packages/utils/index.ts
@@ -1,13 +1,8 @@
 import { version as __VERSION__ } from './package.json';
+import * as utils from './src/js/index';
 
 export const version = __VERSION__;
 
-export * from './src/js/dom';
-export * from './src/js/bom';
-export * from './src/js/reg';
-export * from './src/js/url';
-export * from './src/js/cache';
-export * from './src/js/utils';
-export * from './src/js/array';
-export * from './src/js/random';
-export * from './src/js/format';
+export * from './src/js/index';
+
+export default utils;
