c24f848374415974374170e1655729196edf3a55
shuisheng <2274751790@qq.com>
2022-12-02 13:28:57 +0800

    feat: postinstall脚本

---
+++ packages/components/postinstall.js
@@ -0,0 +1,13 @@
+console.log(
+  '\n' +
+    '\u001B[96mThank you for using @huangshuisheng/components (\u001B[94m https://github.com/galaxy-s10/billd-monorepo/tree/master/packages/components \u001B[96m)\u001B[0m' +
+    '\n\n' +
+    '\u001B[96m>\u001B[94m Author github: https://github.com/galaxy-s10 \u001B[0m' +
+    '\n' +
+    '\u001B[96m>\u001B[94m Author blog:   https://www.hsslive.cn \u001B[0m' +
+    '\n' +
+    '\u001B[96m>\u001B[94m Author npm:    https://www.npmjs.com/~huangshuisheng \u001B[0m' +
+    '\n\n' +
+    '\u001B[96mIf @huangshuisheng/components works for you, welcome to star ~\u001B[0m' +
+    '\n'
+);
---
+++ packages/hooks/postinstall.js
@@ -0,0 +1,13 @@
+console.log(
+  '\n' +
+    '\u001B[96mThank you for using @huangshuisheng/hooks (\u001B[94m https://github.com/galaxy-s10/billd-monorepo/tree/master/packages/hooks \u001B[96m)\u001B[0m' +
+    '\n\n' +
+    '\u001B[96m>\u001B[94m Author github: https://github.com/galaxy-s10 \u001B[0m' +
+    '\n' +
+    '\u001B[96m>\u001B[94m Author blog:   https://www.hsslive.cn \u001B[0m' +
+    '\n' +
+    '\u001B[96m>\u001B[94m Author npm:    https://www.npmjs.com/~huangshuisheng \u001B[0m' +
+    '\n\n' +
+    '\u001B[96mIf @huangshuisheng/hooks works for you, welcome to star ~\u001B[0m' +
+    '\n'
+);
